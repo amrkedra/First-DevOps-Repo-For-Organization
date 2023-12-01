f5dc744bdd2bf88746d07aa4720252f48c8fcb36
amrkedra <amrkedra.1993@gmail.com>
2023-12-01 14:24:42 +0200

    update

--- Terraform/Ec2-instance#1.tf
+++ Terraform/Ec2-instance#1.tf
@@ -1,8 +1,16 @@
+
+
 resource "aws_instance" "web-server"{
     ami = "ami-0f8d2a6080634ee69"
     instance_type = "t3.micro"
+    key_name = "keypair-bahrain"
+    subnet_id = "subnet-028e2eb09b7de9c6a"
+    associate_public_ip_address = true
     tags = {
         name = "test-instance"
         env = "dev"
     }
 }
+
+
+
